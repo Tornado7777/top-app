3a03ca9b14a25b18d1b4ad31d7d5e17855f6648e
Ivan <tornadod@mail.ru>
2022-09-19 01:06:47 +0300

    заголовки + кнопки

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MyTop - our best top</title>
+      <link rel="preconnect" href='https://fonts.gstatic.com' />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
